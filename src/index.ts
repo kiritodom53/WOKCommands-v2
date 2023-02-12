a4c4f504cc5ac70c4929ad2e6bb964f3c99dfc7a
Dominik Mandinec <mandinec53@gmail.com>
2023-02-12 05:44:48 +0100

    Dssb

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import WOK from "./DCMD";
 import CommandType from "./util/CommandType";
 import CooldownTypes from "./util/CooldownTypes";
 import DefaultCommands from "./util/DefaultCommands";
 import DbModels from "./models/index-model";
 import ConfigType from "./util/ConfigType";
+import DssbCmdler from "./DCMD";
 
-module.exports = WOK;
+module.exports = DssbCmdler;
 module.exports.CommandType = CommandType;
 module.exports.ConfigType = ConfigType;
 module.exports.CooldownTypes = CooldownTypes;
